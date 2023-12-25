e318a608652f6db09a489d3a5643bf83683a8fb3
asifmuntasir <shuyeb.dk@gmail.com>
2023-12-25 19:17:23 +0600

    initialize nav and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/SharedComponents/Footer";
+import Navbar from "../pages/SharedComponents/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 }
