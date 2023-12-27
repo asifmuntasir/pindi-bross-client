37f63c87fa29b812dc1592cd64bb76161e3529ac
asifmuntasir <shuyeb.dk@gmail.com>
2023-12-27 20:35:51 +0600

    add parallax on featured section

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,12 +4,13 @@ import featuredImage from '../../../assets/home/featured.jpg';
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitile
                 subHeading={"Check it out"}
                 heading={"Featured Item"}
             ></SectionTitile>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center
+            bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImage} alt="" />
                 </div>
@@ -29,7 +30,7 @@ const Featured = () => {
                         nostrum vel id dolor quibusdam minima
                         maxime blanditiis et in voluptate? Pariatur.
                     </p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">Order Now</button>
                 </div>
             </div>
         </div>
