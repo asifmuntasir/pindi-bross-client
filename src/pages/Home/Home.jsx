da3266812292ff95b8ce6846bc270a5a996e270b
asifmuntasir <shuyeb.dk@gmail.com>
2023-12-29 20:29:20 +0600

    setup dynamic route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import Banner from '../Banner/Banner';
 import Category from './Catergory/Category';
 import Featured from './Featured/Featured';
@@ -7,6 +8,9 @@ import Reviews from './Reviews/Reviews';
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Pindi Bross | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Pindi Bross | Menu</title>
+            </Helmet>
+            <h2>Menu page</h2>
+        </div>
+    );
+}
+
+export default Menu;
